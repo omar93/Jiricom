09c615c8f8f2ca803d4848a4f2071332154cb439
Omar Alhamad <omar93@hotmail.se>
2021-07-04 15:46:17 +0200

    table sorting implemented with an arrow as indicator for the order on the TH element

--- client/src/components/main/table.svelte
+++ client/src/components/main/table.svelte
@@ -1,12 +1,9 @@
 <script>
     import { dataStore } from "../../stores/dataStore"
     import{ onMount } from 'svelte'
+    import  { sortTableByColumn } from '../../lib/sortingTable'
 
     let items = []
-    let current = 0
-    let offset = 7
-    let pages = 0
-    let totalPages = 0
     let table,h
     let mockItems = [
         'timeStart',
@@ -18,66 +15,36 @@
     ]
     dataStore.subscribe(data => {
         if(data.data.length > 0) {
-            pages = 1
-            totalPages = Math.round((data.data.length/7)+1)
-            items = data.data.slice(current,offset)
+            items = data.data
         }
     })
 
     onMount(() => {
-        h = (table.parentElement.clientHeight / 15)-2
-        // console.log(h)
+        h = (table.parentElement.clientHeight / 14)
+        document.querySelectorAll(`th`).forEach(headerCell => {
+            console.log(headerCell)
+            headerCell.addEventListener("click", () => {
+                const tableElement = headerCell.parentElement.parentElement.parentElement
+                const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell)
+                const currentIsAscending = headerCell.classList.contains('th-sort-asc')
+                sortTableByColumn(tableElement, headerIndex, !currentIsAscending)
+            })
+        })
     })
 
-    const handleRight = () => {
-        if(pages < totalPages) {
-            pages++
-            current = offset
-            offset += 7
-            dataStore.subscribe(data => {
-                items = data.data.slice(current,offset)
-                // console.log(items)
-            })
-        }
-
-    }
-    const handleLeft = () => {
-        if(pages > 1) {
-            pages--
-            offset = current
-            current -= 7
-            dataStore.subscribe(data => {
-                items = data.data.slice(current,offset)
-                // console.log(items)
-            })
-        } else {
-            alert('no more data')
-        }
-
-    }
-
-    const changePage = () => {
-        // console.log('Current page: ', pages)
-        current = 0
-        offset = 7
-        current += 1 + (7 * parseInt(pages))
-        offset += 1 + (7 * parseInt(pages))
-        dataStore.subscribe(data => {
-                items = data.data.slice(current,offset)
-                // console.log(items)
-            })
-    }
 
 </script>
 
 <div class="tableContainer" bind:this={table}>
     <table id="customers">
         {#if items.length != 0}
+        <thead>
             <tr>
                 {#each [...Object.keys(items[0])].slice(2, 8) as key}
                     <th> {key} </th>
                 {/each}
             </tr>
+            </thead>
         {:else}
         <tr>
             {#each mockItems as head}
@@ -87,6 +54,7 @@
         {/if}
 
         {#if items.length != 0}
+        <tbody>
             {#each items as item}
                 <tr>
                     {#each [...Object.values(item)].slice(2, 8) as [...columnItem]}
@@ -94,6 +62,7 @@
                     {/each}
                 </tr>
             {/each}
+        </tbody>
         {:else}
         
             {#each {length:7} as nothing}
@@ -106,46 +75,12 @@
             {/each}
         {/if}
     </table>
-    <div class="bottomContainer">
-        <span on:click={handleLeft}>ᐊ</span>
-        {#if pages > 0}
-            <input type="text" bind:value={pages} on:change={changePage}>
-        {:else}
-            <span>{pages}</span>
-        {/if}
-        /
-        <span>{totalPages}</span>
-        <span on:click={handleRight}>ᐅ</span>
-    </div>
 </div>
 
 <style>
     *{
         font-size: 15px;
     }
-
-    input[type=text] {
-        width: 50px;
-        font-size: 25px;
-        height: 25px;
-        margin-top: 10px;
-        text-align: center;
-    }
-
-    .bottomContainer {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: auto;
-        margin-top: 10px;
-        gap: 20px;
-    }
-
-    span {
-        font-size: 25px;
-    }
-
-    span:hover {cursor: pointer;}
 
     .tableContainer {
         width: 97%;
@@ -188,5 +123,6 @@
         position: sticky;
         top: -1px;
         height: 10px;
+        cursor: pointer;
     }
 </style>
